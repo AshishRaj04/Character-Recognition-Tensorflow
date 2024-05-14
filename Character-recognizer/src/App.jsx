9b10a76715ece83e20dbd3032d3a72da21c3170a
Ashish Raj <21c47@bcebakhtiyarpur.org>
2024-05-14 21:31:58 +0530

    added react app

---
+++ Character-recognizer/src/App.jsx
@@ -0,0 +1,13 @@
+import Feed from "./components/Feed"
+
+function App() {
+  
+  return (
+    <>
+      <h1>Character Recognizer</h1>
+      <Feed />
+    </>
+  )
+}
+
+export default App
---
+++ Character-recognizer/src/components/Feed.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import * as tf from "@tensorflow/tfjs";
+import { loadGraphModel } from "@tensorflow/tfjs-converter";
+
+const Feed = () => {
+  const MODEL_URL = "";
+
+  
+  return <div>Feed</div>;
+};
+
+export default Feed;
